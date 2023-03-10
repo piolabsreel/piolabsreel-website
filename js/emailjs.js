03ed2d723bd44c5ec2d6bcf981f49308556f453f
ZSO <seanokai@hotmail.com>
2023-03-10 16:26:31 +0100

    js adjust
    js adjust

--- js/emailjs.js
+++ js/emailjs.js
@@ -1,22 +1,22 @@
 function SendMail() {
   var params = {
-    from_name: document.getElementById("name").value,
-    email_id: document.getElementById("email").value,
-    message: document.getElementById("messages").value,
+    from_name: document.getElementById('name').value,
+    email_id: document.getElementById('email').value,
+    message: document.getElementById('messages').value,
   };
 
-  const serviceID = "service_iu0w43m";
-  const templateID = "template_9205z1j";
+  const serviceID = 'service_iu0w43m';
+  const templateID = 'template_9205z1j';
 
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("messages").value = "";
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('messages').value = '';
       console.log(res);
-      alert("Your mail was sent succesfully");
-  })
+      alert('Your mail was sent succesfully');
+    })
   .catch((err) => console.log(err));
 
 }
