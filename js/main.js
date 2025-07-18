fa2739248a6159660e311a135fbf154b760085df
s.o <seanokai@hotmail.com>
2025-07-18 14:34:22 +0100

    Package form upd
    package form upd

--- js/main.js
+++ js/main.js
@@ -113,7 +113,9 @@ let currentIndex = 0;
       } else if (selected === "sell") {
         result.innerHTML = "Perfect! Check out our <a href='/sell'>Ad & Commercial Video Production</a> for businesses.";
       } else if (selected === "customer") {
-        result.innerHTML = "Alright! Use <a href='#contact-scroll2'>Contact Form</a> | info@piolabsreel.com.";
+        result.innerHTML = "Ofcourse we can! We would like to hear about your project that involes 'alot'!";
+      } else if (selected === "customer2") {
+        result.innerHTML = "Alright! Use <a href='#contact-scroll2'>Contact Form</a> or email info@piolabsreel.com.";
       } else {
         result.innerHTML = "Please choose an option above.";
       }
